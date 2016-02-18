1294cd8f61fc52ada35bc4beb5efbc78810a98c1
yuki <yuki@yuki-PC>
2016-02-18 17:28:23 +0800

    1728

--- src/Bag.java
+++ src/Bag.java
@@ -3,32 +3,47 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Bag {
-	public static int obj[][]={{2,6},{2,3},{6,5},{5,4},{4,6}};
+	public static int objects[][]={{5,4},{2,3},{6,5},{2,6},{4,6}};
 	public static Integer bagspace=10;
 	private static List<List<Status>> stas=new ArrayList<>();
 	private static List<Status> sta=null;
 	private static Status s=null;
-	private static Status pre=null;
 	public static void main(String[] args){
-		for (int i = 0; i < obj.length; i++) {
+
+		for (int i = 0; i < objects.length; i++) {
 			sta=new ArrayList<>();			
 			if (i==0) {
-				for (int j = 0; j < bagspace; j++) {
+				for (int j = 0; j < bagspace+1; j++) {
 					s=new Status();
-					s.space=bagspace;
+					s.space=j;
 					s.putobject(i);
 					sta.add(s);
+					//System.out.println("ccc");
 				}
 			}else {
-				for (int j = 0; j < bagspace; j++) {
-					pre=stas.get(i-1).get(j);
+				for (int j = 0; j < bagspace+1; j++) {
 					s=new Status();
-					s.space=pre.space;
-					s.value=pre.value;
-					s.objectindexes.addAll(pre.objectindexes);
+					s.space=j;
 					if (s.putobject(i)==0) {
-						s.replaceobj(i);
+						s.space=stas.get(i-1).get(j).space;
+						s.value=stas.get(i-1).get(j).value;
+						s.objectindexes.addAll(stas.get(i-1).get(j).objectindexes);
+						//System.out.println("ccc");
 					} else {
+						s.objectindexes.remove(0);
+						if (stas.get(i-1).get(j).value>=(stas.get(i-1).get(j-objects[i][0]).value+objects[i][1])) {
+							s.space=stas.get(i-1).get(j).space;
+							s.value=stas.get(i-1).get(j).value;
+							s.objectindexes.addAll(stas.get(i-1).get(j).objectindexes);
+							
+						} else {
+							s.space=j;
+							s.putobject(i);
+							for (int j2 = 0; j2 < stas.get(i-1).get(j-objects[i][0]).objectindexes.size(); j2++) {
+								
+								s.putobject(stas.get(i-1).get(j-objects[i][0]).objectindexes.get(j2));
+							}
+						}
 						
 					}
 					sta.add(s);
@@ -36,8 +51,8 @@ public class Bag {
 			}
 			stas.add(sta);
 		}		
-		for (int i = 0; i < stas.get(4).get(9).objectindexes.size(); i++) {
-			System.out.println(stas.get(4).get(9).objectindexes.get(i));
+		for (int i = 0; i < stas.get(4).get(10).objectindexes.size(); i++) {
+			System.out.println(stas.get(4).get(10).objectindexes.get(i));
 		}
 	}
 
--- src/Status.java
+++ src/Status.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Status {
-	public int space;
+	public int space=0;
 	public List<Integer> objectindexes=new ArrayList<>();
 	public int value=0;
 	
@@ -11,10 +11,10 @@ public class Status {
 	 * 把序号为objectindex的物体放入背包，
 	 * 计算背包的价值和剩余空间。*/
 	public int putobject(int objectindex){
-		if (space>=Bag.obj[objectindex][0]) {
+		if (space>=Bag.objects[objectindex][0]) {
 			objectindexes.add(objectindex);
-			space=space-Bag.obj[objectindex][0];
-			value=value+Bag.obj[objectindex][1];
+			space=space-Bag.objects[objectindex][0];
+			value=value+Bag.objects[objectindex][1];
 			return 1;
 		} else {
 			return 0;
@@ -27,9 +27,9 @@ public class Status {
 	 *把背包中最后放入的物体换成序号为objectindex的物体。 */
 	public int replaceobj(int objectindex){
 		int lastobjectindex=objectindexes.size()-1;
-		int afterreplacespace=getspace(objectindexes)+Bag.obj[lastobjectindex][0]-Bag.obj[objectindex][0];
-		int beforereplacevalue=getvalue(objectindexes);
-		int afterreplacevalue=getvalue(objectindexes)-Bag.obj[lastobjectindex][1]+Bag.obj[objectindex][1];
+		int afterreplacespace=space+Bag.objects[lastobjectindex][0]-Bag.objects[objectindex][0];
+		int beforereplacevalue=value;
+		int afterreplacevalue=value-Bag.objects[lastobjectindex][1]+Bag.objects[objectindex][1];
 		if ((afterreplacespace>=0)&&(afterreplacevalue>beforereplacevalue)) {
 			objectindexes.remove(lastobjectindex);
 			objectindexes.add(objectindex);
@@ -41,10 +41,10 @@ public class Status {
 		}
 	}
 	
-	public int getvalue(List<Integer> os){
+	/*public int getvalue(List<Integer> os){
 		int v=0;
 		for (int i = 0; i < os.size(); i++) {
-			v=v+Bag.obj[os.get(i)][1];
+			v=v+Bag.objects[os.get(i)][1];
 		}
 		return v;
 	}
@@ -52,8 +52,8 @@ public class Status {
 	public int getspace(List<Integer> os){
 		int s=Bag.bagspace;
 		for (int i = 0; i < os.size(); i++) {
-			s=s-Bag.obj[os.get(i)][0];
+			s=s-Bag.objects[os.get(i)][0];
 		}
 		return s;
-	}
+	}*/
 }
